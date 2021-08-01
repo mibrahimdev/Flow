75dc6b8cf7dfe8d617cd6ecd387735749d3f9748
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-08-01 14:33:36 +0200

    add figma colors

--- app/src/main/java/io/github/mohamedisoliman/flow/ui/theme/Color.kt
+++ app/src/main/java/io/github/mohamedisoliman/flow/ui/theme/Color.kt
@@ -6,3 +6,17 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+object Figma {
+
+    val Green = Color(0xFF07E092)
+    val Blue = Color(0xFF3D4ABA)
+    val Pink = Color(0xFFFD5B71)
+    val LightGrey = Color(0xFFFAFAFF)
+    val Black = Color(0xFF070417)
+    val Purple = Color(0xFF9B51E0)
+    val Orange = Color(0xFFFFA656)
+    val DarkBlack = Color(0xFF18152C)
+    val Background = Color(0xFFFAFAFF)
+}
