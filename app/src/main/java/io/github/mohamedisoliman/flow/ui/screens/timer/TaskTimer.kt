c9aed44f58bc33dd77c7ff52a698bf77c373ac5b
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-08-18 14:09:58 +0200

    add timer buttons

--- app/src/main/java/io/github/mohamedisoliman/flow/ui/screens/timer/TaskTimer.kt
+++ app/src/main/java/io/github/mohamedisoliman/flow/ui/screens/timer/TaskTimer.kt
@@ -1,14 +1,20 @@
 package io.github.mohamedisoliman.flow.ui.screens.timer
 
 import android.text.format.DateUtils
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconToggleButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,6 +23,7 @@ import io.github.mohamedisoliman.flow.R
 import io.github.mohamedisoliman.flow.testing.tasks
 import io.github.mohamedisoliman.flow.ui.*
 import io.github.mohamedisoliman.flow.ui.screens.home.Task
+import io.github.mohamedisoliman.flow.ui.screens.home.TaskTag
 import io.github.mohamedisoliman.flow.ui.theme.Figma
 import io.github.mohamedisoliman.flow.ui.theme.radial
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -24,12 +31,14 @@ import kotlinx.coroutines.ObsoleteCoroutinesApi
 
 const val interval = 1000
 
+@OptIn(ObsoleteCoroutinesApi::class)
 @Preview(showBackground = true)
 @Composable
 fun PreviewTaskTimer() {
     TaskTimer(taskId = 40)
 }
 
+@ObsoleteCoroutinesApi
 @Composable
 fun TaskTimer(taskId: Int?) {
     val task = tasks.firstOrNull { it.id == taskId }
@@ -42,8 +51,8 @@ fun TimerContainer(modifier: Modifier = Modifier, task: Task?) {
     val taskState = remember { task }
     val tag = taskState?.tags?.firstOrNull()
 
-    val targetTime = 60L * 10
-    val lastPause = 60L * 7
+    val targetTime = 60L * 3
+    val lastPause = 60L * 2
     val tick = remember { mutableStateOf(lastPause) }
 
     startTimer(targetTime, lastPause) {
@@ -51,26 +60,11 @@ fun TimerContainer(modifier: Modifier = Modifier, task: Task?) {
     }
 
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            tag?.let {
-                TagView(
-                    tagName = it.name,
-                    tagColor = it.color,
-                    tagStyle = TagStyle.Bordered
-                )
-            }
-
-            PinToggle()
-        }
-
+        TimerHeader(tag)
 
         Column(modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -79,43 +73,103 @@ fun TimerContainer(modifier: Modifier = Modifier, task: Task?) {
 
             if (task != null) {
                 Text(text = task.name, style = MaterialTheme.typography.h5)
-
                 ProjectView(
                     projectName = task.project.name,
                     projectTint = task.project.color
                 )
             }
 
-            Box(
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .fillMaxWidth()
-            ) {
+            TimerCountdownContainer(modifier, targetTime, lastPause, tick)
+        }
 
-                CountDownCircle(
-                    modifier = modifier
-                        .padding(24.dp)
-                        .size(300.dp)
-                        .padding(top = 24.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
-                        .align(Alignment.Center),
-                    targetTimeInSeconds = targetTime,
-                    currentTimeInSeconds = lastPause
-                )
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = tick.value.formatDuration(),
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Medium)
-                )
-
-            }
-
+        Row(
+            modifier = modifier
+                .padding(45.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CircleButton(id = R.drawable.pause_filled)
+            CircleButton(id = R.drawable.stop_filled)
         }
 
 
     }
 
 
+}
+
+@Composable
+private fun TimerCountdownContainer(
+    modifier: Modifier,
+    targetTime: Long,
+    lastPause: Long,
+    tick: MutableState<Long>,
+) {
+    Box(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+    ) {
+
+        CountDownCircle(
+            modifier = modifier
+                .padding(24.dp)
+                .size(300.dp)
+                .padding(top = 24.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
+                .align(Alignment.Center),
+            targetTimeInSeconds = targetTime,
+            currentTimeInSeconds = lastPause
+        )
+        Text(
+            modifier = Modifier
+                .align(Alignment.Center),
+            text = tick.value.formatDuration(),
+            style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Medium)
+        )
+
+    }
+}
+
+@Composable
+private fun TimerHeader(tag: TaskTag?) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        tag?.let {
+            TagView(
+                tagName = it.name,
+                tagColor = it.color,
+                tagStyle = TagStyle.Bordered
+            )
+        }
+
+        PinToggle()
+    }
+}
+
+@Composable
+private fun CircleButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    @DrawableRes id: Int,
+) {
+
+    IconButton(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(color = MaterialTheme.colors.surface)
+            .size(60.dp),
+        onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(id = id),
+            contentDescription = ""
+        )
+    }
 }
 
 fun Long.formatDuration(): String = if (this < 60) {
