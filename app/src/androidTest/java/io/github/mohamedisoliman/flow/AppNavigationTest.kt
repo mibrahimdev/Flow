46fc1bf0ac9937609ff34a29b0a048211c873a0f
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2023-08-25 02:02:12 +0300

    feat: generate dark & light themes screenshots, update README.md

--- app/src/androidTest/java/io/github/mohamedisoliman/flow/AppNavigationTest.kt
+++ app/src/androidTest/java/io/github/mohamedisoliman/flow/AppNavigationTest.kt
@@ -1,6 +1,7 @@
 package io.github.mohamedisoliman.flow
 
 import android.content.Context
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -25,36 +26,50 @@ class AppNavigationTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-
     private lateinit var navController: TestNavHostController
 
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
-
     @Before
     fun setup() {
+
+    }
+
+    private fun setupAppWithTheme(isDark: @Composable () -> Boolean = { false }) {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current).apply {
                 navigatorProvider.addNavigator(ComposeNavigator())
             }
 
-            FlowApp(navController = navController, darkTheme = { true })
+            FlowApp(navController = navController, darkTheme = isDark)
         }
     }
 
     @Test
     fun is_home_screen_displayed_at_first() {
+        setupAppWithTheme()
         //Home screen
         val screenName = Screen.Home.route
 
         val route = navController.currentBackStackEntry?.destination?.route
         assertEquals(route, screenName)
         Screengrab.screenshot(screenName)
+    }
 
+    @Test
+    fun is_home_screen_displayed_at_first_dark() {
+        setupAppWithTheme(isDark = { true })
+        //Home screen
+        val screenName = Screen.Home.route
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, screenName)
+        Screengrab.screenshot(screenName + "_dark")
     }
 
     @Test
     fun clicking_item_card_should_display_timer_screen() {
+        setupAppWithTheme()
         val taskName = tasks.first()
         val tag = taskName.tags.first().name
 
@@ -63,13 +78,37 @@ class AppNavigationTest {
         Screengrab.screenshot(Screen.TaskTimer.route)
     }
 
+    @Test
+    fun clicking_item_card_should_display_timer_screen_dark() {
+        setupAppWithTheme(isDark = { true })
+
+        val taskName = tasks.first()
+        val tag = taskName.tags.first().name
+
+        composeTestRule.onNodeWithContentDescription(taskName.name).performClick()
+        composeTestRule.onNodeWithContentDescription(tag).assertIsDisplayed()
+        Screengrab.screenshot(Screen.TaskTimer.route + "_dark")
+    }
 
     @Test
     fun clicking_report_icon_should_navigate_to_report_screen() {
+        setupAppWithTheme()
         val screen = Screen.Report.route
         val reportScreenTitle = context.getString(R.string.reportScreenTitle)
         composeTestRule.onNodeWithContentDescription(screen).performClick()
         composeTestRule.onNodeWithContentDescription(reportScreenTitle).assertIsDisplayed()
         Screengrab.screenshot(Screen.Report.route)
     }
+
+    @Test
+    fun clicking_report_icon_should_navigate_to_report_screen_dark() {
+        setupAppWithTheme(isDark = { true })
+
+        val screen = Screen.Report.route
+        val reportScreenTitle = context.getString(R.string.reportScreenTitle)
+        composeTestRule.onNodeWithContentDescription(screen).performClick()
+        composeTestRule.onNodeWithContentDescription(reportScreenTitle).assertIsDisplayed()
+        Screengrab.screenshot(Screen.Report.route + "_dark")
+    }
+
 }
